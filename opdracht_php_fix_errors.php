4c6b431dabeb67d9f95c7a72def43ea8e08afa14
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-01-22 16:39:00 +0100

    final

---
+++ opdracht_php_fix_errors.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- saved from url=(0067)http://localhost/websites/chatapp/opdracht_php_fix_errors%20(1).php -->
+<html wtx-context="EBAF25A6-5F35-48BE-B82B-1A5ACE63FB89"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>PHP assignment by:Esmeralda Tijhoff(fix PHP syntax errors)</title>
+
+  </head>
+
+  <body>
+
+      41.2412  
+  
+
+</body></html>
